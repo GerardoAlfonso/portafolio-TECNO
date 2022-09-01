79c7ac7e6e95fbdf64554c5e27eaadbbe9e6719f
GerardoAlfonso <Gerardoalfonso_@hotmail.com>
2022-08-31 19:33:05 -0600

    primer periodo finalizado

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,5 @@
 
+// add class active to show modal with img
 document.querySelectorAll(".modal-container img").forEach(item => {
     item.addEventListener("click", function(event){
         event.stopPropagation();
@@ -6,9 +7,51 @@ document.querySelectorAll(".modal-container img").forEach(item => {
     })
 })
 
+// delete class active to hide modal
 document.querySelectorAll(".modal-container").forEach(item => {
     item.addEventListener("click", function(event){
         event.stopPropagation();
         this.classList.remove("active");
     })
+})
+
+
+// button for show Activities 
+document.querySelectorAll(".modal-container img").forEach(item => {
+    item.addEventListener("click", function(event){
+        event.stopPropagation();
+        this.parentNode.classList.add("active");
+    })
+})
+
+// show items of Class exercise
+document.getElementById("AC").addEventListener("click", function(event){
+    event.stopPropagation();
+    this.parentNode.parentNode.classList.add("active");
+    document.getElementById("class").classList.remove("active");
+
+    document.getElementById("back").parentNode.parentNode.classList.remove("active");
+})
+
+
+// show items of book exercise
+document.getElementById("AL").addEventListener("click", function(event){
+    event.stopPropagation();
+    this.parentNode.parentNode.classList.add("active");
+    document.getElementById("book").classList.remove("active");
+
+    // show button Back
+    document.getElementById("back").parentNode.parentNode.classList.remove("active");
+})
+
+// back list class or book exercise
+document.getElementById("back").addEventListener("click", function(event){
+    event.stopPropagation();
+    this.parentNode.parentNode.classList.add("active");
+    document.getElementById("class").classList.add("active");
+    document.getElementById("book").classList.add("active");
+
+    // show button Back
+    document.getElementById("AC").parentNode.parentNode.classList.remove("active");
+
 })
